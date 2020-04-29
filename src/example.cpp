92eea6ebcfc4a163f6713e7401036494258f29cc
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-30 01:38:36 +0300

    added test

--- src/example.cpp
+++ src/example.cpp
@@ -1,21 +1,45 @@
 #include "example.h"
 
-Dummy::Dummy() {
-
+string multremoval(const char* file*)
+{
+    setlocale(LC_ALL, "Russian");
+    freopen(file, "r", stdin);
+    int n, t, c;
+	vector<int> v;
+	vector<int>:: iterator it;
+	vector<int>:: reverse_iterator rit;
+	string str="";
+	//cout << "??????? ?????????? ?????????" << endl;
+	cin >> n;
+	for (int i = 0; i<n; i++){
+        cin >>t;
+        v.push_back(t);
+    }
+	for (it = v.begin(); it != v.end(); it++){
+        t = *it;
+        c = count(it, v.end(), t);
+        if (c-1)
+            for (rit = v.rbegin(); rit != v.rend(); rit++)
+                if ((c-1) && (*rit == t)) {
+                    v.erase(rit.base()-1);
+                    v.push_back(0);
+                    c--;
+                }
+	}
+	string str2="";
+    for (it=v.begin();it!=v.end();it++)
+    {
+        str+=to_string(*it);
+        str+=" ";
+    }
+    return str;
 }
-
-bool Dummy::doSomething() {
-    // Do silly things, using some C++17 features to enforce C++17 builds only.
-    constexpr int digits[2] = {0, 1};
-    auto [zero, one] = digits;
-    return zero + one;
-}
-
 
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #include "doctest.h"
 TEST_CASE("we can have tests written here, to test impl. details")
 {
-    CHECK(true);
+    string str1="5 6 7 8 9 0 0 0 0 0 ";
+    CHECK(multremoval("../app/in2.txt")==str1);
 }
 #endif
