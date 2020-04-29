db6e11e90aa5bfe8afe0721370e40d0087add78a
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-29 23:12:41 +0300

    update main.cpp with working files

--- app/main.cpp
+++ app/main.cpp
@@ -6,30 +6,84 @@
 #include "doctest.h"
 #endif
 
-#include <iostream>
-#include <stdlib.h>
 
+#include <stdlib.h>
 #include "exampleConfig.h"
 #include "example.h"
+#include <iostream>
+#include <vector>
+#include <iomanip>
 
-/*
- * Simple main program that demontrates how access
- * CMake definitions (here the version number) from source code.
- */
+using namespace std;
 int main() {
-  std::cout << "C++ Boiler Plate v"
-            << PROJECT_VERSION_MAJOR
-            << "."
-            << PROJECT_VERSION_MINOR
-            << "."
-            << PROJECT_VERSION_PATCH
-            << "."
-            << PROJECT_VERSION_TWEAK
-            << std::endl;
-  std::system("cat ../LICENSE");
+    int n, m;
+    freopen("./in1.txt", "r", stdin)
+    cin >> n >> m;
+    vector <vector<int> > a(n, vector<int>(m, 0));
+    vector < vector<int> >::iterator row;
+    vector < vector<int> >::reverse_iterator rerow;
+    vector <int>::iterator col;
+    vector <int>::reverse_iterator recol;
 
-  // Bring in the dummy class from the example source,
-  // just to show that it is accessible from main.cpp.
-  Dummy d = Dummy();
-  return d.doSomething() ? 0 : -1;
+
+// улитка
+    int num = 0 , k = 0;
+    row = a.begin();
+    col = row->begin();
+    while (num <= n*m) {
+        for (col = (a.begin()+k)->begin()+k; col != (a.begin()+k)->end()-k; col++){
+            num++;
+            *col = num;
+        };
+        if (num == n*m) break;
+        for (row = a.begin()+k+1; row != a.end()-k; row++){
+            num++;
+            *(row->end()-k-1) = num;
+        };
+        if (num == n*m) break;
+        for (recol = (a.rbegin()+k)->rbegin()+k+1; recol != (a.rbegin()+k)->rend()-k; recol++){
+            num++;
+            *(recol) = num;
+        };
+        if (num == n*m) break;
+        for (rerow = a.rbegin()+k+1; rerow != a.rend()-k-1; rerow++){
+            num++;
+            *((rerow->rend()-k-1)) = num;
+        };
+        if (num == n*m) break;
+        k++;
+    }
+
+    cout << endl;
+    for (row = a.begin(); row != a.end(); row++){
+        for (col = row->begin(); col != row->end(); col++)
+            cout <<setw(3)<< *col<< "  ";
+        cout << endl;
+    }
+
+//не улитка
+    vector<int> rowp(m,0);
+    num = 0;
+    row = a.begin();
+    for (col = row->begin(); col != row->end(); col++){
+        num++;
+        *col = num;
+    }
+    rowp = *row;
+    for (row = a.begin()+1; row != a.end(); row++){
+        *row = rowp;
+        row->pop_back();
+        row->insert(row->begin(), *(row->begin())+1);
+        rowp = *row;
+    }
+    cout << endl;
+
+
+    for (row = a.begin(); row != a.end(); row++){
+        for (col = row->begin(); col != row->end(); col++)
+            cout <<setw(3)<< *col<< "  ";
+        cout << endl;
+    }
+
+    return 0;
 }
