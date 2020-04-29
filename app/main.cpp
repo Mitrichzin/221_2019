ad0706c7326cf8aef3a8e07317e68c6b9ae583c2
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-30 00:45:29 +0300

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -16,85 +16,8 @@
 
 using namespace std;
 int diagon(int n,int m)
-{
-    vector <vector<int> > a(n, vector<int>(m, 0));
-    vector < vector<int> >::iterator row;
-    vector < vector<int> >::reverse_iterator rerow;
-    vector <int>::iterator col;
-    vector <int>::reverse_iterator recol;
-    vector<int> rowp(m,0);
-    int num = 0;
-    row = a.begin();
-    for (col = row->begin(); col != row->end(); col++){
-        num++;
-        *col = num;
-    }
-    rowp = *row;
-    for (row = a.begin()+1; row != a.end(); row++){
-        *row = rowp;
-        row->pop_back();
-        row->insert(row->begin(), *(row->begin())+1);
-        rowp = *row;
-    }
-
-    int sum=0;
-    for (row = a.begin(); row != a.end(); row++){
-        for (col = row->begin(); col != row->end(); col++)
-        {
-            cout <<setw(3)<< *col<< "  ";
-            sum+=*col;
-        }
-        cout << endl;
-    }
-
-    return sum;
-}
 int ulitka(int n, int m)
-{
-    vector <vector<int> > a(n, vector<int>(m, 0));
-    vector < vector<int> >::iterator row;
-    vector < vector<int> >::reverse_iterator rerow;
-    vector <int>::iterator col;
-    vector <int>::reverse_iterator recol;
 
-    int num = 0 , k = 0;
-    row = a.begin();
-    int deb=0;
-    col = row->begin();
-    while (num <= n*m) {
-        deb+=(num+1);
-        for (col = (a.begin()+k)->begin()+k; col != (a.begin()+k)->end()-k; col++){
-            num++;
-            *col = num;
-        };
-        if (num == n*m) break;
-        for (row = a.begin()+k+1; row != a.end()-k; row++){
-            num++;
-            *(row->end()-k-1) = num;
-        };
-        if (num == n*m) break;
-        for (recol = (a.rbegin()+k)->rbegin()+k+1; recol != (a.rbegin()+k)->rend()-k; recol++){
-            num++;
-            *(recol) = num;
-        };
-        if (num == n*m) break;
-        for (rerow = a.rbegin()+k+1; rerow != a.rend()-k-1; rerow++){
-            num++;
-            *((rerow->rend()-k-1)) = num;
-        };
-        if (num == n*m) break;
-        k++;
-    }
-
-    cout << endl;
-    for (row = a.begin(); row != a.end(); row++){
-        for (col = row->begin(); col != row->end(); col++)
-            cout <<setw(3)<< *col<< "  ";
-        cout << endl;
-    }
-    cout << endl;
-    return deb;
-}
 int main() {
     int n, m;
     freopen("./in1.txt", "r", stdin);
