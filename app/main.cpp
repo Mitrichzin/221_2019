1b433dfa021d16a090e1d5030b7a901391f79fcc
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-30 01:30:57 +0300

    main.cpp initialized

--- app/main.cpp
+++ app/main.cpp
@@ -5,18 +5,51 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 #endif
-
-#include <iostream>
 #include <stdlib.h>
 
 #include "exampleConfig.h"
 #include "example.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-/*
- * Simple main program that demontrates how access
- * CMake definitions (here the version number) from source code.
- */
-int main() {
+using namespace std;
+
+string multremoval(const char* file*)
+{
+    setlocale(LC_ALL, "Russian");
+    freopen(file, "r", stdin);
+    int n, t, c;
+	vector<int> v;
+	vector<int>:: iterator it;
+	vector<int>:: reverse_iterator rit;
+	string str="";
+	cin >> n;
+	for (int i = 0; i<n; i++){
+        cin >>t;
+        v.push_back(t);
+    }
+	for (it = v.begin(); it != v.end(); it++){
+        t = *it;
+        c = count(it, v.end(), t);
+        if (c-1)
+            for (rit = v.rbegin(); rit != v.rend(); rit++)
+                if ((c-1) && (*rit == t)) {
+                    v.erase(rit.base()-1);
+                    v.push_back(0);
+                    c--;
+                }
+	}
+	string str2="";
+    for (it=v.begin();it!=v.end();it++)
+    {
+        str+=to_string(*it);
+        str+=" ";
+    }
+    return str;
+}
+int main()
+{
   std::cout << "C++ Boiler Plate v"
             << PROJECT_VERSION_MAJOR
             << "."
@@ -26,10 +59,7 @@ int main() {
             << "."
             << PROJECT_VERSION_TWEAK
             << std::endl;
-  std::system("cat ../LICENSE");
-
-  // Bring in the dummy class from the example source,
-  // just to show that it is accessible from main.cpp.
-  Dummy d = Dummy();
-  return d.doSomething() ? 0 : -1;
+  std::system("cat ../LICENSE");  
+  cout<<multremoval();
+  return 0;
 }
