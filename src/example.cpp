2eb6c34a078ccedf7e428b811417cd41cf2a2aab
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-30 12:00:22 +0300

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -8,6 +8,17 @@
 #include <string>
 #include <algorithm>
 using namespace std;
+
+Dummy::Dummy() {
+
+}
+
+bool Dummy::doSomething() {
+    // Do silly things, using some C++17 features to enforce C++17 builds only.
+    constexpr int digits[2] = {0, 1};
+    auto [zero, one] = digits;
+    return zero + one;
+}
 string multremoval(const char* file)
 {
     freopen(file, "r", stdin);
