2a87f72b0cfe50b5ffbe652d9ff9da4316a6ec0d
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-30 16:29:56 +0300

    init source

--- src/example.cpp
+++ src/example.cpp
@@ -1,9 +1,33 @@
 #include "example.h"
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 Dummy::Dummy() {
-
+using namespace std;
 }
+void print(pair<float, pair<int, int> > a )
+{
+    cout << a.second.first << " " << a.second.second << endl;
+}
+vector< pair<float, pair<int, int> > > sortdots(const char* input) 
+{
+	freopen(input,"r", stdin);
+	int n,a,b;
+    float c;
+    cin >> n;
 
+    vector< pair<float, pair<int, int> > > A;
+
+    for (int i=0;i<n;i++)
+    {
+        cin >> a >> b;
+        c = a * a + b * b;
+        A.push_back(make_pair(c, make_pair(a, b)));
+    }
+
+    sort(A.begin(), A.end());
+    return A;
+}
 bool Dummy::doSomething() {
     // Do silly things, using some C++17 features to enforce C++17 builds only.
     constexpr int digits[2] = {0, 1};
