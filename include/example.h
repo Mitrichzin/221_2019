fbfd127d8670fc62dbe848879f701e5aa7c01213
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-30 17:00:43 +0300

    Update example.h

--- include/example.h
+++ include/example.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "example.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility> 
 /**
  * This is a dummy class to demonstrate features of the boiler plate.
  */
@@ -17,7 +22,9 @@ class Dummy {
   bool doSomething();
 };
 
-
+using namespace std;
+void print(pair<float, pair<int, int> > a );
+vector< pair<float, pair<int, int> > > sortdots(const char* input);
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #include "doctest.h"
 TEST_CASE("we can have tests in headers if we want")
