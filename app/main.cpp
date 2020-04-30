7eae92c23abc0216355ddf356b98936550983ada
Mitrichzin <58105096+Mitrichzin@users.noreply.github.com>
2020-04-30 12:07:29 +0300

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -15,37 +15,7 @@
 #include <string>
 using namespace std;
 
-string multremoval(const char* file)
-{
-    freopen(file, "r", stdin);
-    int n, t, c;
-	vector<int> v;
-	vector<int>:: iterator it;
-	vector<int>:: reverse_iterator rit;
-	string str="";
-	cin >> n;
-	for (int i = 0; i<n; i++){
-        cin >>t;
-        v.push_back(t);
-    }
-	for (it = v.begin(); it != v.end(); it++){
-        t = *it;
-        c = count(it, v.end(), t);
-        if (c-1)
-            for (rit = v.rbegin(); rit != v.rend(); rit++)
-                if ((c-1) && (*rit == t)) {
-                    v.erase(rit.base()-1);
-                    v.push_back(0);
-                    c--;
-                }
-	}
-    for (it=v.begin();it!=v.end();it++)
-    {
-        str+=to_string(*it);
-        str+=" ";
-    }
-    return str;
-}
+string multremoval(const char* file);
 int main()
 {
   std::cout << "C++ Boiler Plate v"
